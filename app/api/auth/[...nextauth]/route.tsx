4eff3d87afe258fdcd80883100156448ab386b3c
Goshawk <stefan73parisianspv@hotmail.com>
2025-02-25 05:48:56 +0100

    Google SignIn

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,13 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const authOptons: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!,
+    }),
+  ],
+};
+
+const handler = (req: any, res: any) => NextAuth(req, res, authOptons);
+export { handler as GET, handler as POST };
---
+++ app/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type { ThemeProviderProps } from "next-themes";
+
+import * as React from "react";
+import { HeroUIProvider } from "@heroui/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { store } from "@/store";
+import { Provider } from "react-redux";
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <SessionProvider>
+      <Provider store={store}>
+        <HeroUIProvider navigate={router.push}>
+          <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </HeroUIProvider>
+      </Provider>
+    </SessionProvider>
+  );
+}
